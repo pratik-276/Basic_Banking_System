eb24f56d5828837d7faa8c3290e7ed4f90e11ca6
Pratik Mukherjee <pratikmukherjee32@gmail.com>
2021-04-09 21:44:34 +0530

    transaction state added

--- src/containers/Transactions/Transaction/Transaction.js
+++ src/containers/Transactions/Transaction/Transaction.js
@@ -3,12 +3,19 @@ import classes from './Transaction.module.css';
 
 
 const Transaction = (props) => {
+    let border = null;
+    if(props.transaction.state==="success"){
+        border = {borderRight: "5px solid green"}
+    }else if(props.transaction.state==="failure"){
+        border = {borderRight: "5px solid red"}
+    }
     return (
-        <div className={classes.custRow1}>
+        <div className={classes.custRow1} style={border}>
             <p>{props.index}</p>
             <p>{props.transaction.sender}</p>
             <p>{props.transaction.receiver}</p>
             <p>{props.transaction.amount}</p>
+            <p>{props.transaction.state}</p>
         </div>
     );
 }
